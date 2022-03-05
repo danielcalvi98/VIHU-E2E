0c1260feb08efa8024620b51e12daadf89972971
Daniel <danielc19@ru.is>
2022-03-05 01:43:12 +0000

    react-testing

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,11 +1,73 @@
 import React from "react";
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
 import "@testing-library/jest-dom";
 import Home from "../pages/index";
-
 // Good starting point: https://testing-library.com/docs/react-testing-library/example-intro
 
 // TODO setup your mock api here
+const server = setupServer(
+  rest.get('/api/list', (req, res, ctx) => {
+    return res(ctx.status(200),ctx.json([{id: "1", title: "TODO"}]))
+  }),
+  rest.post('/api/add', (req, res, ctx) => {
+    return res(ctx.status(200))
+  }),
+
+  rest.delete('/api/remove', (req, res, ctx) => {
+    return res(ctx.status(200))
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 describe("Tests", () => {
   // TODO Add your react-testing-library tests here
+  test("Display loading", async () => {
+    
+    server.use(
+      rest.get('/api/list', (req, res, ctx) => {
+        return res(ctx.json(null))
+      }),
+    )
+    render(<Home />)
+    await waitFor(() => 
+      expect(screen.getByTestId("loading")).toBeInTheDocument()
+    );  
+  })
+
+  test("Check if 1 item is in the list", async () => {
+    render(<Home />)
+    await waitFor(() =>
+        expect(screen.getAllByTestId("todo-item")).toHaveLength(1)
+    );
+  })
+
+  test("Add TODO to the list", async () => {
+    render(<Home />);
+    await waitFor(() =>
+        expect(screen.getAllByTestId("todo-item")).toHaveLength(1)
+    );
+    fireEvent.change(screen.getByTestId("todo-input"), {
+        target: { value: "TODO" },
+    });
+    fireEvent.submit(screen.getByTestId("todo-input"));
+    await waitFor(() =>
+        expect(screen.getAllByTestId("todo-item")).toHaveLength(2)
+    );
+});
+// Write a test that removes an item from the list
+test("Remove TODO from the list", async () => {
+    render(<Home />);
+    await waitFor(() =>
+        expect(screen.getAllByTestId("todo-item")).toHaveLength(1)
+    );
+    fireEvent.click(screen.getByTestId("todo-item"));
+    await waitFor(() =>
+        expect(screen.queryAllByTestId("todo-item")).toHaveLength(0)
+    );
+  });
 });
--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -2,34 +2,45 @@
 
 describe("📝 TODO app", () => {
   // TODO Add your Cypress tests here
+  before(() => {
+    cy.exec('npm run prisma:reset')
+  })
+
+  after(() => {
+    cy.exec('npm run prisma:reset')
+  })
+
   beforeEach(() => {
     cy.visit('http://localhost:3000')  
   })
 
-  after(() => {
-    cy.get('[data-testid=todo-item]').click({multiple:true})
+  describe("TODO list is empty", () => {
+    it("should validate if the TODO list is empty", () => {
+      cy.get('[data-testid=todo-item]').should('have.length', 0)      
+    })
   })
 
-  it("should validate if the TODO list is empty", () => {
-    cy.get('[data-testid=todo-item]').should('have.length', 0)
-    
-  })
-
-  it('should add a new item to TODO list', () => {
-    const newTodo = 'Finish E2E project 😎'
-    cy.get('[data-testid=todo-input]').type(`${newTodo}{enter}`);
-    cy.get('[data-testid=todo-item]').should('have.length',1)
+  
+  describe("Add to TODO list", () => {
+    it('should add a new item to TODO list', () => {
+      const newTodo = 'Finish E2E project 😎'
+      cy.get('[data-testid=todo-input]').type(`${newTodo}{enter}`);
+      cy.get('[data-testid=todo-item]').should('have.length',1)
+    })
   })
   
-
-  it('should add another item to TODO list', () => {
-    const newTodo = 'Have a cold refreshing beverage 🍹'
-    cy.get('[data-testid=todo-input]').type(`${newTodo}{enter}`);
-    cy.get('[data-testid=todo-item]').should('have.length', 2)
+  describe("Add another", () => {
+    it('should add another item to TODO list', () => {
+      const newTodo = 'Have a cold refreshing beverage 🍹'
+      cy.get('[data-testid=todo-input]').type(`${newTodo}{enter}`);
+      cy.get('[data-testid=todo-item]').should('have.length', 2)
+    })
   })
 
-  it('should remove one item from the TODO list', () => {
-    cy.get('[data-testid=todo-item]').first().click()
-    cy.get('[data-testid=todo-item]').should('have.length',1)
+  describe("Remove from TODO list", () => {
+    it('should remove one item from the TODO list', () => {
+      cy.get('[data-testid=todo-item]').first().click()
+      cy.get('[data-testid=todo-item]').should('have.length',1)
+    })
   })
 });
